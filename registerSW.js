2bb2e0fa9f24a7d045063013086b431da2030a2c
dan gisyynys <isauragisyamid@gmail.com>
2026-06-29 20:00:38 +0700

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/todo-listg/sw.js', { scope: '/todo-listg/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
